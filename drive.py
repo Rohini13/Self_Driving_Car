3342d8c43e16c3b3a0abbdd561bbee475e948d6c
Rohini Mishra <iit2018087@iiita.ac.in>
2019-06-30 17:55:45 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,84 +1,57 @@
-#parsing command line arguments
 import argparse
-#decoding camera images
 import base64
-#for frametimestamp saving
 from datetime import datetime
-#reading and writing files
 import os
-#high level file operations
 import shutil
-#matrix math
 import numpy as np
-#real-time server
 import socketio
-#concurrent networking 
 import eventlet
-#web server gateway interface
 import eventlet.wsgi
-#image manipulation
 from PIL import Image
-#web framework
 from flask import Flask
-#input output
 from io import BytesIO
-
-#load our saved model
 from keras.models import load_model
+import utils #helper class
 
-#helper class
-import utils
-
-#initialize our server
 sio = socketio.Server()
-#our flask (web) app
 app = Flask(__name__)
-#init our model and image array as empty
 model = None
 prev_image_array = None
 
-#set min/max speed for our autonomous car
 MAX_SPEED = 25
 MIN_SPEED = 10
 
-#and a speed limit
 speed_limit = MAX_SPEED
 
-#registering event handler for the server
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
-        # The current steering angle of the car
         steering_angle = float(data["steering_angle"])
-        # The current throttle of the car, how hard to push peddle
+       
         throttle = float(data["throttle"])
-        # The current speed of the car
         speed = float(data["speed"])
-        # The current image from the center camera of the car
         image = Image.open(BytesIO(base64.b64decode(data["image"])))
         try:
-            image = np.asarray(image)       # from PIL image to numpy array
-            image = utils.preprocess(image) # apply the preprocessing
-            image = np.array([image])       # the model expects 4D array
+            image = np.asarray(image)      
+            image = utils.preprocess(image)
+            image = np.array([image])       
 
-            # predict the steering angle for the image
+            
             steering_angle = float(model.predict(image, batch_size=1))
-            # lower the throttle as the speed increases
-            # if the speed is above the current speed limit, we are on a downhill.
-            # make sure we slow down first and then go back to the original max speed.
+            
             global speed_limit
             if speed > speed_limit:
-                speed_limit = MIN_SPEED  # slow down
+                speed_limit = MIN_SPEED  
             else:
                 speed_limit = MAX_SPEED
-            throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2
+            throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2 #just a magic function :P
 
             print('{} {} {}'.format(steering_angle, throttle, speed))
             send_control(steering_angle, throttle)
         except Exception as e:
             print(e)
 
-        # save frame
+       
         if args.image_folder != '':
             timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
             image_filename = os.path.join(args.image_folder, timestamp)
@@ -120,7 +93,7 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    #load model
+   
     model = load_model(args.model)
 
     if args.image_folder != '':
@@ -134,8 +107,11 @@ if __name__ == '__main__':
     else:
         print("NOT RECORDING THIS RUN ...")
 
-    # wrap Flask application with engineio's middleware
-    app = socketio.Middleware(sio, app)
+    
+    app = socketio.Middleware(sio, app)# handling multiple sockets
 
     # deploy as an eventlet WSGI server
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
+    #The Web Server Gateway Interface (WSGI) is a simple calling convention 
+    # for web servers to forward requests to web applications or frameworks 
+    # written in the Python programming language.
